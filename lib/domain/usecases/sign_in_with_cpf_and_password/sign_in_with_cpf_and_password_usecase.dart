b9df28097d32ccdc4a9a160fc10e9b6070c619e5
Daniel Marciano <daniel.castro09@hotmail.com>
2022-09-06 05:37:02 -0300

    🔥 Remove: Removed files

--- lib/domain/usecases/sign_in_with_cpf_and_password/sign_in_with_cpf_and_password_usecase.dart
+++
@@ -1,8 +0,0 @@
-import 'package:either_dart/either.dart';
-
-import '../../entities/user_entity.dart';
-
-abstract class SignInWithCpfAndPasswordUsecase {
-  Future<Either<Exception, UserEntity>> call(
-      {required String cpfNumber, required String password});
-}
